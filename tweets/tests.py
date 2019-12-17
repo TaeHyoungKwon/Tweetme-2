4006116dfe147844954bfec240369b816aab020a
TaeHyoungKwon <kth5604@gmail.com>
2019-12-17 21:48:50 +0900

    add testcase about tweet_detail_view

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 
+from tweets.models import Tweet
 from tweets.views import home_view
 
 
@@ -11,7 +12,13 @@ class TestTweetView(TestCase):
         response = self.c.get("/")
         self.assertEqual(response.content.decode("ascii"), "<h1>Hello World</h1>")
 
-    def test_tweet_detail_view_should_return_hello_world_text_with_tweet_id(self):
-        tweet_id = "1"
-        response = self.c.get("/tweets/" + tweet_id)
-        self.assertEqual(response.content.decode("ascii"), "<h1>Hello 1</h1>")
+    def test_tweet_detail_view_should_raise_404_when_given_tweet_id_is_not_exist(self):
+        Tweet.objects.create(content="TweetMe")
+        not_exist_tweet_id = "not_exist_tweet_id"
+        response = self.c.get("/tweets/" + not_exist_tweet_id)
+        self.assertEqual(response.status_code, 404)
+
+    def test_tweet_detail_view_should_return_hello_string_with_tweet_id_and_obj_content(self):
+        tweet_instance = Tweet.objects.create(content="TweetMe")
+        response = self.c.get("/tweets/" + str(tweet_instance.pk))
+        self.assertEqual(response.content.decode("ascii"), "<h1>Hello 1 - TweetMe</h1>")
