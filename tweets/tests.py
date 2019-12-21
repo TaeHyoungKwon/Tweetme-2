02cd02feb06ac652e4f5598264703d97d5ee011b
TaeHyoungKwon <kth5604@gmail.com>
2019-12-22 01:34:07 +0900

    add testcase about serialize

--- tweets/tests.py
+++ tweets/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase, Client
 
 from tweets.models import Tweet
 
+MAX_LIKES_COUNT = 200
+MIN_LIKES_COUNT = 0
+
 
 class TestTweetView(TestCase):
     def setUp(self):
@@ -46,26 +49,37 @@ class TestTweetView(TestCase):
         response = self.c.get("/tweets/")
         json_data = response.json()
         self.assertEqual(response.status_code, 200)
-        self.assertLessEqual(json_data["response"][0]["likes"], 122)
-        self.assertGreaterEqual(json_data["response"][0]["likes"], 0)
+        self.assertLessEqual(json_data["response"][0]["likes"], MAX_LIKES_COUNT)
+        self.assertGreaterEqual(json_data["response"][0]["likes"], MIN_LIKES_COUNT)
 
     def test_tweet_create_view_should_return_200_when_request_method_is_not_POST(self):
         Tweet.objects.create(content="TweetMe")
         response = self.c.get("/create-tweet/")
         self.assertEqual(response.status_code, 200)
 
-
     def test_tweet_create_view_should_redirect_when_next_url_is_exist(self):
         Tweet.objects.create(content="TweetMe")
-        next_url = '/'
-        response = self.c.post("/create-tweet/", data={
-            'next': '/',
-        })
+        next_url = "/"
+        response = self.c.post("/create-tweet/", data={"next": "/"})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, next_url)
 
     def test_tweet_create_view_should_return_200(self):
         Tweet.objects.create(content="TweetMe")
-        response = self.c.post("/create-tweet/", data={
-        })
+        response = self.c.post("/create-tweet/", data={})
         self.assertEqual(response.status_code, 200)
+
+    def test_tweet_create_view_should_be_201_when_given_request_is_ajax(self):
+        response = self.c.post("/create-tweet/", HTTP_X_REQUESTED_WITH="XMLHttpRequest", data={"next": "/"})
+        self.assertEqual(response.status_code, 201)
+
+    def test_tweet_create_view_should_return_serialized_json_when_given_request_is_ajax(self):
+        content = "KwonTaeHyoung"
+        response = self.c.post(
+            "/create-tweet/", HTTP_X_REQUESTED_WITH="XMLHttpRequest", data={"next": "/", "content": content}
+        )
+        res = response.json()
+        self.assertEqual(res["id"], 1)
+        self.assertEqual(res["content"], content)
+        self.assertLessEqual(res["likes"], MAX_LIKES_COUNT)
+        self.assertGreaterEqual(res["likes"], MIN_LIKES_COUNT)
