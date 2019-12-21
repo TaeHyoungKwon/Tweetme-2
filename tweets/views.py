5e78b1667ce1416aacc8157990ee7751b042275d
TaeHyoungKwon <kth5604@gmail.com>
2019-12-21 10:58:57 +0900

    add likes value 0 to 122

--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse, Http404, JsonResponse
+import random
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from tweets.models import Tweet
 
 
 def home_view(request):
-    return render(request, 'pages/home.html', context={}, status=200)
+    return render(request, "pages/home.html", context={}, status=200)
 
 
 def tweet_detail_view(request, tweet_id):
@@ -22,9 +24,6 @@ def tweet_detail_view(request, tweet_id):
 
 def tweet_list_view(request):
     tweets = Tweet.objects.all()
-    tweet_list = [{'id': tweet.id, 'content': tweet.content} for tweet in tweets]
-    tweet_list_data = {
-        'isUser': False,
-        'response': tweet_list,
-    }
+    tweet_list = [{"id": tweet.id, "content": tweet.content, "likes": random.randint(0, 122)} for tweet in tweets]
+    tweet_list_data = {"isUser": False, "response": tweet_list}
     return JsonResponse(tweet_list_data, status=200)
