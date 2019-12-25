eb5f3be597be8ed89b65c63e611a73255b113bed
TaeHyoungKwon <kth5604@gmail.com>
2019-12-25 13:42:47 +0900

    add tweetadmin class

--- tweets/admin.py
+++ tweets/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from tweets.models import Tweet
 
-admin.site.register(Tweet)
+
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "user"]
+    search_fields = ["content", "user__username", "user__eamil"]
+
+    class Meta:
+        model = Tweet
+
+
+admin.site.register(Tweet, TweetAdmin)
