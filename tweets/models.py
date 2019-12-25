876b3597cfb183590194291f167208c139281223
TaeHyoungKwon <kth5604@gmail.com>
2019-12-25 17:00:07 +0900

    delete user in model

--- tweets/models.py
+++ tweets/models.py
@@ -7,7 +7,6 @@ User = settings.AUTH_USER_MODEL
 
 
 class Tweet(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
 
