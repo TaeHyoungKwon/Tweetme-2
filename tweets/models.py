0b7e506a43c006c5c3ed055c3fe3a44900550e2b
TaeHyoungKwon <kth5604@gmail.com>
2019-12-25 13:38:19 +0900

    add column in tweet model

--- tweets/models.py
+++ tweets/models.py
@@ -1,18 +1,18 @@
 import random
 
+from django.conf import settings
 from django.db import models
+
+User = settings.AUTH_USER_MODEL
 
 
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
 
     class Meta:
-        ordering = ['-id']
+        ordering = ["-id"]
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "content": self.content,
-            "likes": random.randint(0, 200)
-        }
+        return {"id": self.id, "content": self.content, "likes": random.randint(0, 200)}
