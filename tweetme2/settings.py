0784c49e9dbfa801258fcd1fda6b9514132ac4fb
TaeHyoungKwon <kth5604@gmail.com>
2019-12-28 12:46:42 +0900

    add settings

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -125,4 +125,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
+
 MAX_TWEET_LENGTH = 240
